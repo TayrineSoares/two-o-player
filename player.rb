dd6e2caf4ede1f4891b22ea09916b5d4db1a3b7a
TayrineSoares <tayrinecristina@hotmail.com>
2025-04-15 12:45:11 -0400

    Add methods to player class

--- player.rb
+++ player.rb
@@ -7,14 +7,21 @@ class Player
   end
 
   # PUBLIC METHODS
-  def lose_life 
 
+  # Reduces one life when the player answers incorrectly
+  def lose_life
+    @lives -= 1
   end
 
-  
+  # Checks if the player is still alive
+  def alive?
+    @lives > 0
+  end
 
-
-
+  # Displays the player's current score (name and remaining lives)
+  def score_display
+    puts "#{@name}: #{@lives}/3"
+  end
 end
 
 
